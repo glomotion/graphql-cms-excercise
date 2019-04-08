2eb6628ef1672c07dfbf7c9ecc8e2caf958075d1
tim <tim@glo.id.au>
2019-04-09 09:15:27 +1000

    dont use classes - swap state logic out for hook usage

--- client/components/faqs/Faqs.jsx
+++ client/components/faqs/Faqs.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Query } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown/with-html';
@@ -17,51 +17,42 @@ const FAQS_DATA = gql`
   }
 `;
 
-class Faqs extends Component {
-  state = {
-    currentFaq: 0,
-  };
+const Faqs = () => {
+  const [currentFaqIndex, setCurrentFaqIndex] = useState(0);
+  return (
+    <Query query={FAQS_DATA}>
+      {({ loading, error, data: { faqs } }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
 
-  componentDidMount() {
-    console.log('faqs component did mount!');
-  }
+        return (
+          <div className={styles.faqsContainer}>
+            <h1 className={styles.faqsContainer__heading}>Some frequently asked questions</h1>
 
-  render() {
-    return (
-      <Query query={FAQS_DATA}>
-        {({ loading, error, data: { faqs } }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-
-          return (
-            <div className={styles.faqsContainer}>
-              <h1 className={styles.faqsContainer__heading}>Some frequently asked questions</h1>
-
-              <div className={styles.faqsContainer__columnLayout}>
-                <div className={styles.faqsContainer__columnLayout__body}>
-                  <Faq id={this.state.currentFaq} />
-                </div>
-
-                <aside className={styles.sidebar}>
-                  {faqs.map(({ title }, index) => (
-                    <a
-                      key={index}
-                      onClick={() => this.setState({ currentFaq: index })}
-                      className={classNames(styles.sidebar__item, {
-                        [styles['sidebar__item--active']]: this.state.currentFaq === index,
-                      })}
-                    >
-                      {title}
-                    </a>
-                  ))}
-                </aside>
+            <div className={styles.faqsContainer__columnLayout}>
+              <div className={styles.faqsContainer__columnLayout__body}>
+                <Faq id={currentFaqIndex} />
               </div>
+
+              <aside className={styles.sidebar}>
+                {faqs.map(({ title }, index) => (
+                  <a
+                    key={index}
+                    onClick={() => setCurrentFaqIndex(index)}
+                    className={classNames(styles.sidebar__item, {
+                      [styles['sidebar__item--active']]: currentFaqIndex === index,
+                    })}
+                  >
+                    {title}
+                  </a>
+                ))}
+              </aside>
             </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default Faqs;
--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import Icon from 'client/components/icon/Icon';
@@ -7,48 +7,40 @@ import { homeUrl, faqsUrl } from 'client/utils/page-urls';
 
 import styles from 'client/components/header/header.module.scss';
 
-class Header extends Component {
-  state = {
-    menuOpen: false,
-  };
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <header
+      className={classNames(styles.header, {
+        [styles['header--menuOpen']]: menuOpen,
+      })}
+    >
+      <Link to={homeUrl()} className={styles.header__logo}>
+        <img
+          className={styles.header__logo__image}
+          src="client/static/images/logo--qantas.svg"
+          alt="Qantas Logo"
+        />
+      </Link>
 
-  render() {
-    return (
-      <header
-        className={classNames(styles.header, {
-          [styles['header--menuOpen']]: this.state.menuOpen,
+      <nav
+        className={classNames(styles.nav, {
+          [styles['nav--menuOpen']]: menuOpen,
         })}
       >
-        <Link to={homeUrl()} className={styles.header__logo}>
-          <img
-            className={styles.header__logo__image}
-            src="client/static/images/logo--qantas.svg"
-            alt="Qantas Logo"
-          />
+        <Link to={homeUrl()} className={styles.nav__navItem}>
+          <span className={styles.nav__navItem__inner}>Home</span>
         </Link>
+        <Link to={faqsUrl()} className={styles.nav__navItem}>
+          <span className={styles.nav__navItem__inner}>Faqs</span>
+        </Link>
+      </nav>
 
-        <nav
-          className={classNames(styles.nav, {
-            [styles['nav--menuOpen']]: this.state.menuOpen,
-          })}
-        >
-          <Link to={homeUrl()} className={styles.nav__navItem}>
-            <span className={styles.nav__navItem__inner}>Home</span>
-          </Link>
-          <Link to={faqsUrl()} className={styles.nav__navItem}>
-            <span className={styles.nav__navItem__inner}>Faqs</span>
-          </Link>
-        </nav>
-
-        <button
-          className={styles.header__menuButton}
-          onClick={() => this.setState({ menuOpen: !this.state.menuOpen })}
-        >
-          <Icon type="menu" />
-        </button>
-      </header>
-    );
-  }
-}
+      <button className={styles.header__menuButton} onClick={() => setMenuOpen(!menuOpen)}>
+        <Icon type="menu" />
+      </button>
+    </header>
+  );
+};
 
 export default Header;
